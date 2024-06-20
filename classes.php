15219f6bf782b2680414a1c844a7667c391979bf
Bismark Arroyo <bis.arroyo@gmail.com>
2024-06-20 09:55:43 -0600

    create initial code for user handle

---
+++ classes.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+class handle_user {
+  public function __construct(
+    readonly public string $email,
+    private string $name,
+    public string $password
+  ){}
+
+  public function login() {
+    $this->email; 
+  }
+
+  public function signup() {
+
+  }
+
+  public function logout() {
+
+  }
+}
